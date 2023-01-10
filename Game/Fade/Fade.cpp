dc293683b210dfad3dd2d2bcbc53e3823f68a974
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-01-10 15:44:33 +0900

    FadeInの追加

--- Game/Fade/Fade.cpp
+++ Game/Fade/Fade.cpp
@@ -1,9 +1,21 @@
 #include "Game/Fade/Fade.h"
+#include "Game/MyMath/MyMath.h"
 
 Fade::Fade() {
-
+	red = 0;
+	green = 0;
+	blue = 0;
+	alpha = 0;
 }
 
-unsigned int Fade::FadeIn(unsigned int color) {
+unsigned int Fade::FadeIn(unsigned int color,float speed) {
+	red = color >> 24;
+	green = color >> 16 & 0xff;
+	blue = color >> 8 & 0xff;
+	alpha = color & 0xff;
+	
+	alpha += speed;
+	MyMath::Clamp((float)alpha, 255.0f, 0.0f);
 
+	return MyMath::GetRGB(red, green, blue, alpha);
 }
--- Game/Fade/Fade.h
+++ Game/Fade/Fade.h
@@ -3,8 +3,14 @@
 class Fade {
 public:
 	Fade();
+private:
+	unsigned int red;
+	unsigned int green;
+	unsigned int blue;
+	unsigned int alpha;
+
 public:
-	unsigned int FadeIn(unsigned int color);
+	unsigned int FadeIn(unsigned int color,float speed);
 
 	unsigned int FadeOut(unsigned int color);
 };
