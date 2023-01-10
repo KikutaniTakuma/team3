24fc89ebbd36abbc0734c235e04b49f34210a655
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-01-10 17:47:02 +0900

    Flashの追加

--- Game/Fade/Fade.cpp
+++ Game/Fade/Fade.cpp
@@ -6,6 +6,7 @@ Fade::Fade() {
 	green = 0;
 	blue = 0;
 	alpha = 0;
+	flag = false;
 }
 
 unsigned int Fade::FadeIn(unsigned int color,float speed) {
@@ -15,7 +16,7 @@ unsigned int Fade::FadeIn(unsigned int color,float speed) {
 	alpha = color & 0xff;
 	
 	alpha += speed;
-	MyMath::Clamp((float)alpha, 255.0f, 0.0f);
+	MyMath::Clamp(static_cast<float>(alpha), 255.0f, 0.0f);
 
 	return MyMath::GetRGB(red, green, blue, alpha);
 }
@@ -27,7 +28,27 @@ unsigned int Fade::FadeOut(unsigned int color, float speed) {
 	alpha = color & 0xff;
 
 	alpha -= speed;
-	MyMath::Clamp((float)alpha, 255.0f, 0.0f);
+	MyMath::Clamp(static_cast<float>(alpha), 255.0f, 0.0f);
+
+	return MyMath::GetRGB(red, green, blue, alpha);
+}
+
+unsigned int Fade::Flash(unsigned int color, float speed) {
+	red = color >> 24;
+	green = color >> 16 & 0xff;
+	blue = color >> 8 & 0xff;
+	alpha = color & 0xff;
+
+	if (alpha >= 255.0f)
+	{
+		flag = true;
+	}
+	else if (alpha <= 0.0f)
+	{
+		flag = false;
+	}
+
+	flag ? alpha -= speed : alpha += speed;
 
 	return MyMath::GetRGB(red, green, blue, alpha);
 }
