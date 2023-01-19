3b9f3a68709868d9669a7525626eb8ec8692a0a9
aoaomidri <aoaomidori2723@gmail.com>
2023-01-19 13:16:55 +0900

    positionをwolrdPosに変更 SizeをgetSizeに変更

--- Game/GoalUI/GoalUI.cpp
+++ Game/GoalUI/GoalUI.cpp
@@ -2,7 +2,6 @@
 #include"Goal/Goal.h"
 GoalUI::GoalUI(Camera* camera)
 	:Object(camera),
-	UIsize(20.0f),
 	width(1280.0f),
 	height(720.0f),
 	color(0xffffffff)
@@ -16,29 +15,32 @@ GoalUI::~GoalUI() {
 
 void GoalUI::Update() {
 	//ゴールの位置とUIの位置を合わせる
-	positionUI = positionGoal->getPos();
+	pos.worldPos = positionGoal->getPos();
 	//画面の外に出ないようにする処理
 	
-	if (positionUI.x < UIsize) {
-		positionUI.x = UIsize;
+	if (pos.worldPos.x < pos.getSize().x / 2.0f) {
+		pos.worldPos.x = pos.getSize().x / 2.0f;
 	}
-	else if (positionUI.x > width-UIsize) {
-		positionUI.x = width - UIsize;
+	else if (pos.worldPos.x > width - pos.getSize().x / 2.0f) {
+		pos.worldPos.x = width - pos.getSize().x / 2.0f;
 	}
-	if (positionUI.y > height - UIsize) {
-		positionUI.y = height - UIsize;
+	if (pos.worldPos.y > height - pos.getSize().y / 2.0f) {
+		pos.worldPos.y = height - pos.getSize().y / 2.0f;
+		pos.worldPos.y *= -1;
 	}
-	else if (positionUI.y < UIsize) {
-		positionUI.y = UIsize;
+	else if (pos.worldPos.y < pos.getSize().y / 2.0f) {
+		pos.worldPos.y = pos.getSize().y / 2.0f;
+		pos.worldPos.y *= -1;
 	}
-	pos.worldPos = positionUI;
+	
+	pos.worldMatrix.Translate(pos.worldPos);
 }
 
 void GoalUI::Draw(class Texture& tex) {
 	//画面の中にゴールがあるかを判別
 	//存在する場合ゴールを表示し、ない場合UIを表示する
-	if (positionGoal->getPos().x > 0 && positionGoal->getPos().x < width - UIsize) {
-		if (positionGoal->getPos().y > 0 && positionGoal->getPos().y < height - UIsize) {
+	if (positionGoal->getPos().x > 0 && positionGoal->getPos().x < width - pos.getSize().x / 2.0f) {
+		if (positionGoal->getPos().y > 0 && positionGoal->getPos().y < height - pos.getSize().y / 2.0f) {
 			//こっちがゴールを表示
 			camera->DrawUI(drawPos, tex, 0, 0, color);
 		}
