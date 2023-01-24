bfeaf9ad82f7fc94f24a73d1a4a88b2b479bdc4e
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-01-24 11:43:38 +0900

    一時退避

--- SCENE/TITLE/TITLE.cpp
+++ SCENE/TITLE/TITLE.cpp
@@ -1,22 +1,42 @@
 #include "SCENE/TITLE/TITLE.h"
+#include "Game/Fade/Fade.h"
 
 Title::Title(Camera* camera) : Object(camera) {
 	pos.Set({ 640.0f,360.0f }, { 1280.0f,720.0f });
+	sceneFlag = false;
+	BG.Set("./Resources/Title/background.png", 1280, 1280, 720);
+	color = 0x00000000;
 }
 
 Title::~Title() {
 
 }
 
+void Title::SceneChange() {
+	scene = Situation::STAGE;
+}
+
+
 void Title::Update() {
+	//	ƒV[ƒ“Ø‚è‘Ö‚¦
+	if (sceneFlag)
+	{
+		SceneChange();
+	}
+
+	color = Fade::FadeInOut(color, 5.0f, true);
+//	color = Fade::Flash(color, 5.0f);
+
+	//	À•W•ÏŠ·
 	pos.worldMatrix.Translate(pos.worldPos);
 }
 
 void Title::Reset() {
-
+	sceneFlag = false;
 }
 
 void Title::Draw() {
 //	camera->DrawQuad(drawPos, tex, 0, false);
-	camera->DrawUI(drawPos, whiteBox, 0, false, 0x991111ff);
+	camera->DrawUI(drawPos, BG, 0, false, color);
 }
+
--- SCENE/TITLE/TITLE.h
+++ SCENE/TITLE/TITLE.h
@@ -9,14 +9,21 @@ public:
 	~Title();
 
 private:
+	//	シーン切り替え用フラグ
+	bool sceneFlag;
+	//	シーン切り替え
+	void SceneChange();
 
+	Texture BG;
+	unsigned int color;
 public:
-	//
+	//	
 	void Update() override;
 
 	void Reset() override;
 
 	void Draw() override;
 
+	
 };
 
